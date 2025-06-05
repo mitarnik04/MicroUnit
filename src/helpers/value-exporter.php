1bd2425e2e8c6ad3cc06960e55b9a688cf366698
Mitar Nikolic <mitar.mn4@gmail.com>
2025-06-05 23:58:36 +0200

    Fixed closing bracket apperaing on same line if object. Cleanup

--- src/helpers/value-exporter.php
+++ src/helpers/value-exporter.php
@@ -2,6 +2,8 @@
 
 class ValueExporter
 {
+    private const SPACING_UNIT = '   ';
+
     public static function export($var, int $indent = 0)
     {
         return match (true) {
@@ -22,9 +24,11 @@ class ValueExporter
     private static function exportArray(array $vals, int $indent = 0)
     {
         $spacing = self::getSpacing($indent);
+        $innerSpacing = self::getSpacing($indent + 1);
+
         $output = "Array (" . PHP_EOL;
         foreach ($vals as $key => $value) {
-            $output .= $spacing . "    [$key] => ";
+            $output .= $innerSpacing . "[$key] => ";
             $output .= self::export($value, $indent + 1);
             $output .= PHP_EOL;
         }
@@ -34,12 +38,15 @@ class ValueExporter
 
     private static function exportObject(object $obj, int $indent = 0)
     {
-        $class = get_class($obj);
         $spacing = self::getSpacing($indent);
+        $innerSpacing = self::getSpacing($indent + 1);
+
+        $class = get_class($obj);
         $output = "Object of class $class (" . PHP_EOL;
         foreach (get_object_vars($obj) as $prop => $value) {
-            $output .= $spacing . "    [$prop] => ";
+            $output .= $innerSpacing . "[$prop] => ";
             $output .= self::export($value, $indent + 1);
+            $output .= PHP_EOL;
         }
 
         return $output . $spacing . ")";
@@ -47,6 +54,6 @@ class ValueExporter
 
     private static function getSpacing(int $indent)
     {
-        return str_repeat('    ', $indent);
+        return str_repeat(self::SPACING_UNIT, $indent);
     }
 }
