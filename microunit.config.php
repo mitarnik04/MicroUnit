6b855f716c214a855feca9f79774408f8b59f704
Mitar Nikolic <mitar.mn4@gmail.com>
2025-06-21 21:14:47 +0200

    remove wrongly added file.

--- microunit.config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use MicroUnit\Config\MicroUnitConfigBuilder;
-use MicroUnit\Output\StringTestWriter;
-
-return MicroUnitConfigBuilder::create()
-    ->withTestDir('./tmp-tests')
-    ->addTestWriter(new StringTestWriter())
-    ->build();
