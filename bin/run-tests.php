13b196005e778449a7eda2476320a64209590c1c
Mitar Nikolic <mitar.mn4@gmail.com>
2025-06-23 04:31:04 +0200

    move autloading init into own class.

--- bin/run-tests.php
+++ bin/run-tests.php
@@ -1,13 +1,6 @@
 #!/usr/bin/env php
 <?php
 
-const AUTOLOADER_PATH_CACHE_KEY = 'autoloaderPath';
-
-//Using require_once since autoloader isn't available yet here. 
-require_once __DIR__ . '/../src/Helpers/Utils.php';
-require_once __DIR__ . '/../src/Cache/ICache.php';
-require_once __DIR__ . '/../src/Cache/JsonCache.php';
-
 set_exception_handler(function (Throwable $e) {
     error_log($e);
     fwrite(STDERR, 'Something unexpected occured during test execution:' . PHP_EOL);
@@ -16,22 +9,8 @@ set_exception_handler(function (Throwable $e) {
     die(1);
 });
 
-
-$cache = new MicroUnit\Cache\JsonCache('cache');
-
-$autoloader = $cache->get(AUTOLOADER_PATH_CACHE_KEY);
-
-//If the file got moved to a new location or is not cached yet we want to rescan the directories to find it.
-if (is_null($autoloader) || !file_exists($autoloader)) {
-    $autoloader = MicroUnit\Helpers\Utils::findFileInDirectoryOrAbove('vendor/autoload.php', __DIR__);
-    $cache->set(AUTOLOADER_PATH_CACHE_KEY, $autoloader);
-};
-if (is_null($autoloader)) {
-    fwrite(STDERR, 'Could not find the autoloader. Please run "composer install" first.');
-    die(1);
-}
-
-require_once $autoloader;
+require_once __DIR__ . '/../src/Bootstrap/AutoloadingInitializer.php';
+\MicroUnit\Bootstrap\AutoloadingInitializer::initAutoLoadig();
 
 use MicroUnit\Helpers\Utils;
 use MicroUnit\Bootstrap\ConfigInitializer;
@@ -42,7 +21,6 @@ use MicroUnit\Bootstrap\LoggingInitializer;
  * Use in CLI or build pipelines to execute the full test suite.
  */
 
-const SRC_DIR = __DIR__ . '/../src';
 const RUN_LOG_FOLDER = __DIR__ . '/run_logs';
 
 if (!is_dir(RUN_LOG_FOLDER)) {
